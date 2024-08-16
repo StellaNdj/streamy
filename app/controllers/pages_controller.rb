d1886e6307ed3cd07fdf74e56b6f4dd00ada01e4
Stella Ndjikessi <stelladiana@live.fr>
2024-08-16 10:45:33 +0200

    Home method popular movies & tv shows added

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
   def home
-    
+    @popular_movies = Movie.order(rating: :desc).limit(10)
+    @popular_tv_shows = TvShow.order(rating: :desc).limit(10)
   end
 end
