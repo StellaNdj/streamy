d629e0eac4f51dc1d124bc6e5baaa846f22950c7
Stella Ndjikessi <stelladiana@live.fr>
2024-08-19 10:14:52 +0200

    Video test sample

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,7 @@
 class PagesController < ApplicationController
   def home
-    @popular_movies = Movie.order(rating: :desc).limit(10)
-    @popular_tv_shows = TvShow.order(rating: :desc).limit(10)
+    @popular_movies = Movie.order(rating: :desc).limit(10);
+    @popular_tv_shows = TvShow.order(rating: :desc).limit(10);
+    @video_url = '/videos/OnePiece.mp4';
   end
 end
