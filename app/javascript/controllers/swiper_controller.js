6d9496b212ad62210d108c9c984decc9c80a9db1
Stella Ndjikessi <stelladiana@live.fr>
2024-08-21 08:53:27 +0200

    Swiper properly set up and working on homepage

--- app/javascript/controllers/swiper_controller.js
+++ app/javascript/controllers/swiper_controller.js
@@ -5,13 +5,13 @@ export default class extends Controller {
   connect() {
     this.swiper = new Swiper('.swiper-container', {
       // Swiper options go here
-      slidesPerView: 3,
-      spaceBetween: 10,
+      slidesPerView: 4,
+      spaceBetween: 1,
       navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
       },
-      // You can add more options based on your needs
+      
     });
   }
 }
