377f3ed17d1c4c8560bbd36820344eb20b0acf62
Stella Ndjikessi <stelladiana@live.fr>
2024-08-24 11:52:21 +0200

    Different display in movies index

--- app/javascript/controllers/poster_controller.js
+++ app/javascript/controllers/poster_controller.js
@@ -4,7 +4,20 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["image", "wrapper"]
 
-  connect() {
-    console.log('Poster controller ON')
-  }
+  // connect() {
+  //   console.log('Poster controller ON')
+  //   this.colorThief = new ColorThief();
+  //   if (this.imageTarget.complete) {
+  //     this.applyDominantColor();
+  //   } else {
+  //     this.imageTarget.addEventListener("load", () => {
+  //       this.applyDominantColor();
+  //     });
+  //   }
+  // }
+
+  // applyDominantColor() {
+  //   const dominantColor = this.colorThief.getColor(this.imageTarget);
+  //   this.wrapperTarget.style.backgroundColor = `rgb(${dominantColor.join(",")})`;
+  // }
 }
--- app/javascript/controllers/swiper_controller.js
+++ app/javascript/controllers/swiper_controller.js
@@ -5,13 +5,13 @@ export default class extends Controller {
   connect() {
     this.swiper = new Swiper('.swiper-container', {
       // Swiper options go here
-      slidesPerView: 4,
+      slidesPerView: 6,
       spaceBetween: 1,
       navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
       },
-      
+
     });
   }
 }
