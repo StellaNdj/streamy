2cc30afb716f89e1946ee8534672cb6c5461f142
Stella Ndjikessi <stelladiana@live.fr>
2024-08-25 10:20:34 +0200

    Changing the swiper controller to have different slides based on dataset

--- app/javascript/controllers/swiper_controller.js
+++ app/javascript/controllers/swiper_controller.js
@@ -2,10 +2,13 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="swiper"
 export default class extends Controller {
+
   connect() {
+    const slidesPerView = this.element.dataset.slidesPerView;
+
     this.swiper = new Swiper('.swiper-container', {
       // Swiper options go here
-      slidesPerView: 6,
+      slidesPerView: parseInt(slidesPerView, 10),
       spaceBetween: 1,
       navigation: {
         nextEl: '.swiper-button-next',
