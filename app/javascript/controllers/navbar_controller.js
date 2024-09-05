2f8e766cd40084d42d1ded52a67db04958cc2b36
Stella Ndjikessi <stelladiana@live.fr>
2024-09-05 12:04:48 +0200

    User logo and logout added to auth navbar, plus JS to follow navbar colors

--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -2,9 +2,10 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="navbar"
 export default class extends Controller {
-  static targets = ['nav'];
+  static targets = ['nav', 'navLogo'];
   connect() {
     window.addEventListener('scroll', this.handleScroll.bind(this));
+    console.log(this.navLogoTarget);
   }
 
   disconnect() {
@@ -15,8 +16,10 @@ export default class extends Controller {
     if(window.scrollY > 0) {
       this.navTarget.style.backgroundColor = 'rgba(10, 41, 68, 0.8)';
       this.navTarget.style.backdropFilter = 'blur(10px)';
+      this.navLogoTarget.style.borderColor = 'rgba(10, 41, 68, 0.8)';
     } else {
       this.navTarget.style.backgroundColor = '#00050d';
+      this.navLogoTarget.style.borderColor = '#00050d';
     }
   }
 }
