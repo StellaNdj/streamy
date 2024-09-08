86204c6f81309276e6badf9757537e3491763995
Stella Ndjikessi <stelladiana@live.fr>
2024-09-08 10:17:53 +0200

    Swiper resize slides change functionning

--- app/javascript/controllers/swiper_controller.js
+++ app/javascript/controllers/swiper_controller.js
@@ -3,18 +3,56 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="swiper"
 export default class extends Controller {
 
+  initialize() {
+    this.mediaQueries = [
+      { query: "(max-width: 600px)", slidesPerView: 2 },
+      { query: "(min-width: 601px) and (max-width: 1024px)", slidesPerView: 4 },
+      { query: "(min-width: 1025px)", slidesPerView: 6 }
+    ];
+
+    this.swiper = null;
+    this.updateSlidesPerView = this.updateSlidesPerView.bind(this);
+  }
+
   connect() {
-    const slidesPerView = this.element.dataset.slidesPerView;
+    // Add event listeners for each media query
+    this.mediaQueries.forEach(mq => {
+      const mediaQueryList = window.matchMedia(mq.query);
+      mediaQueryList.addListener(this.updateSlidesPerView.bind(this));
+    });
+
+    this.updateSlidesPerView();
+  }
+
+  updateSlidesPerView() {
+    // Find the matching media query
+    const matchingMediaQuery = this.mediaQueries.find(mq => window.matchMedia(mq.query).matches);
+
+    // Update the dataset based on the matching media query
+    if (matchingMediaQuery) {
+      this.element.dataset.slidesPerView = matchingMediaQuery.slidesPerView;
+    }
+
+    // Reinitialize the Swiper instance
+    if (this.swiper && this.swiper.destroy) {
+      this.swiper.destroy(true, true);
+      this.swiper = null;
+    }
+    this.initializeSwiper();
+  }
+
+  initializeSwiper() {
+    const slidesPerView = parseInt(this.element.dataset.slidesPerView, 10);
 
     this.swiper = new Swiper('.swiper-container', {
       // Swiper options go here
-      slidesPerView: parseInt(slidesPerView, 10),
+      slidesPerView: slidesPerView,
       spaceBetween: 1,
       navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
       },
-
     });
   }
+
 }
